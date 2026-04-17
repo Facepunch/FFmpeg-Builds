a734dc788b64bcee17ac20b7312e3b4098b0ffd9
Lorenz Junglas <lorenz.junglas@facepunchstudios.com>
2026-04-17 10:48:55 +0200

    Vulkan VP9 encoding/decoding

--- addins/fp.sh
+++ addins/fp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 FF_CONFIGURE="$FF_CONFIGURE --disable-everything  --disable-amf --disable-audiotoolbox --disable-cuda-llvm  --disable-cuvid --disable-dxva2 --disable-ffnvcodec --disable-libdrm --disable-nvdec --disable-nvenc --disable-v4l2-m2m --disable-vaapi --disable-vdpau --disable-videotoolbox --disable-opencl"
-FF_CONFIGURE="$FF_CONFIGURE --enable-vulkan --enable-hwaccel=av1_vulkan"
-FF_CONFIGURE="$FF_CONFIGURE --enable-decoder=ac3*,opus,vorbis,flac,vp8,libvpx_vp8,vp9,libvpx_vp9,pcm*,apcm*,mp3*,av1,av1_vulkan,libaom_av1,webp,libwebp,libwebp_animwebp"
+FF_CONFIGURE="$FF_CONFIGURE --enable-vulkan --enable-hwaccel=av1_vulkan,vp9_vulkan"
+FF_CONFIGURE="$FF_CONFIGURE --enable-decoder=ac3*,opus,vorbis,flac,vp8,libvpx_vp8,vp9,vp9_vulkan,libvpx_vp9,pcm*,apcm*,mp3*,av1,av1_vulkan,libaom_av1,webp,libwebp,libwebp_animwebp"
 FF_CONFIGURE="$FF_CONFIGURE --enable-encoder=ac3*,opus,libopus,vorbis,flac,vp8,libvpx_vp8,vp9,libvpx_vp9,pcm*,apcm*,mp3*,av1,av1_vulkan,libaom_av1,libsvtav1,webp,libwebp,libwebp_anim"
 FF_CONFIGURE="$FF_CONFIGURE --enable-demuxer=avi,wav,matroska,webm,webp_chunk,mp3,mp4,m4v,mov,3gp,3g2,mj2,ac3,ac4,flac,av1,ogg,hls --enable-muxer=avi,wav,matroska,matroska_audio,webm,webp,mp3,mp4,m4v,mov,3gp,3g2,mj2,ac3,ac4,flac,av1,ogg,hls"
 FF_CONFIGURE="$FF_CONFIGURE --enable-filter=transpose_vulkan,vflip_vulkan,hflip_vulkan,fps,scale"
